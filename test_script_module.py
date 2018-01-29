a055de05f8d5d5e16b7ee912b558b7a84c42e121
leehh <lihaohandavid@gmail.com>
2018-01-29 16:38:19 -0500

    final

--- test_script_module.py
+++ test_script_module.py
@@ -1,10 +1,14 @@
 from bayesianNet import *
 
+'''
 fac1 = Factor(('a', 'b'), {(True, True): 0.1, (False, True): 0.3, (True, False): 0.2, (False, False): 0.5})
+
 fac2 = Factor(('b',), {(True,): 0.6, (False,): 0.9})
+
 fac3 = Factor(('d', 'c', 'b'), {(True, True, True): 0.1, (False, True, True): 0.3, (True, False, True): 0.2,
                                 (False, False, True): 0.5, (True, True, False): 0.1, (False, True, False): 0.3, (True, False, False): 0.2,
                                 (False, False, False): 0.5})
+'''
 
 model = BayesianModel([('a','b'),('a','d'),('c','d')])
 model.add_cpd('a',[[0.1,0.2],[0.3,0.5]],['b'])
